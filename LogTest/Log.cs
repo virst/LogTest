2156fadc9c2332a63d6727fd0b3d7387042cd0c7
Вирстюк Михаил <m.vi@live.com>
2021-09-09 14:27:13 +0500

    First commit

---
+++ LogTest/Log.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+
+namespace LogTest
+{
+    public static class Log
+    {
+
+        public static readonly NLog.Logger Logger;
+
+        static Log()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "${basedir}/Logs/log_${date:format=yyyy-MM-dd}.log" };
+            var errorlogfile = new NLog.Targets.FileTarget("errorlogfile") { FileName = "${basedir}/Logs/err_${date:format=yyyy-MM-dd}.log" };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            config.AddRule(LogLevel.Error, LogLevel.Fatal, errorlogfile);
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+
+            Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        }
+    }
+}
---
+++ LogTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Log.Logger.Info("Hello World!");
+
+            try
+            {
+                for (int i = -4; i < 5; i++)
+                {
+                    int d = 44 / i;
+                    Log.Logger.Info("d = " + d);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error(e);
+            }
+        }
+    }
+}
